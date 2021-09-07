ecf94cfb5d3f9d70bff819ccb5413caa60fe22dd
Tochukwu <tochukwusage4@gmail.com>
2021-09-07 20:27:54 +0100

    added gender

---
+++ OnlineBanking.Domain/Enumerators/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineBanking.Domain.Enumerators
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female,
+        PreferNotToSay
+    }
+}
