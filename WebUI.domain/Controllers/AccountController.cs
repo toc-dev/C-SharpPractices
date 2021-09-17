561a0a6b63a0842dde1dc47ee0534af9f4ef2ef3
Kachii <ugwu_kennedy@icloud.com>
2021-09-17 12:45:56 +0100

    Signup update

--- WebUI.domain/Controllers/AccountController.cs
+++ WebUI.domain/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OnlineBanking.Domain.Entities;
 using System;
@@ -9,6 +10,7 @@ using WebUI.domain.Model;
 
 namespace WebUI.domain.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
@@ -19,12 +21,58 @@ namespace WebUI.domain.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        public ViewResult SignUp()
+
+        [AllowAnonymous]
+        public IActionResult SignUp()
         {
             return View();
         }
 
-        public ViewResult LogIn()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    PhoneNumber = model.PhoneNumber,
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    FullName = $"{model.FirstName} {model.LastName}",
+                    Address = new Address
+                    {
+                        PlotNo = model.PlotNo,
+                        StreetName = model.StreetName,
+                        City = model.State,
+                        State = model.State,
+                        Country = model.Country
+                    }
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ModelState.AddModelError(string.Empty, "Failed signup Attempt");
+
+            }
+            return View(model);
+        }
+
+    public ViewResult LogIn()
         {
             return View();
         }
@@ -41,9 +89,7 @@ namespace WebUI.domain.Controllers
                 {
                     return RedirectToAction("Index", "Home");
                 }
-
                 ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
-
             }
             return View();
         }
---
+++ WebUI.domain/Model/SignUpViewModel.cs
@@ -0,0 +1,56 @@
+﻿using OnlineBanking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.domain.Model
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Required]
+        public int PlotNo { get; set; }
+
+        [Required]
+        public string StreetName { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password to continue")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
+
