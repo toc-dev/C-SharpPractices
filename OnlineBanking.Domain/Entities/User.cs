b20c62d11d6a9f9c8c96e8f020d7488e5605f04a
Kachii <ugwu_kennedy@icloud.com>
2021-09-19 07:42:59 +0100

    added address property

--- OnlineBanking.Domain/Entities/User.cs
+++ OnlineBanking.Domain/Entities/User.cs
@@ -2,7 +2,6 @@
 using OnlineBanking.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace OnlineBanking.Domain.Entities
@@ -10,12 +9,11 @@ namespace OnlineBanking.Domain.Entities
     public class User : IdentityUser, IEntity
     {
         public string FullName { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } 
         public string CreatedBy { get; set; }
         public string UpdatedBy { get; set; }
-        [ForeignKey("Address")]
-        public int AddressID { get; set; }
+        public int AddressId { get; set; }
         public Address Address { get; set; }
     }
 
