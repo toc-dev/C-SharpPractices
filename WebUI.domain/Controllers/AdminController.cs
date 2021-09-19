a8dae91b0efbd4359b51e5688fd6c6f4abca4c24
Kachii <ugwu_kennedy@icloud.com>
2021-09-19 15:39:01 +0100

    Create Role added

--- WebUI.domain/Controllers/AdminController.cs
+++ WebUI.domain/Controllers/AdminController.cs
@@ -1,24 +1,48 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OnlineBanking.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebUI.domain.Model;
 
 namespace WebUI.domain.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppRole _role;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, AppRole role)
         {
             _roleManager = roleManager;
+            _role = role;
         }
+
+
         [HttpGet]
         public IActionResult CreateRole()
         {
+            ViewBag.Title = "Create Role";
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+                _role.Name = model.RoleName;
+            var result = await _roleManager.CreateAsync(_role);
+            if (result.Succeeded)
+                return RedirectToAction("index", "Home"); 
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
         }
     }
 }
