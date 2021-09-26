50709c4997bf5f56a73b49f020c6ee4e2cb77314
Kachii <ugwu_kennedy@icloud.com>
2021-09-26 06:05:09 +0100

    Login bug solved

--- WebUI.domain/Controllers/AccountController.cs
+++ WebUI.domain/Controllers/AccountController.cs
@@ -10,14 +10,15 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace WebUI.domain.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
-            userManager = _userManager;
-            signInManager = _signInManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
         public async Task<IActionResult>  EnrollUser(CreateUserViewModel model)
         {
@@ -57,7 +58,7 @@ namespace WebUI.domain.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
-        public async Task<IActionResult> LogIn(LoginViewModel model, string returnUrl = null)
+        /*public async Task<IActionResult> LogIn(LoginViewModel model, string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             if (ModelState.IsValid)
@@ -98,6 +99,43 @@ namespace WebUI.domain.Controllers
             //        return LocalRedirect(returnUrl);
             //    }
             //}
+        }*/
+
+        public async Task<IActionResult> LogIn(LoginViewModel model, string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                ModelState.AddModelError("", "Invalid login attempt.");
+                        
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                // _logger.LogInformation("User logged in.");
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return LocalRedirect(returnUrl);
+                }
+            }
+            if (result.RequiresTwoFactor)
+            {
+                return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+            }
+            if (result.IsLockedOut)
+            {
+                // _logger.LogWarning("User account locked out.");
+                return RedirectToPage("./Lockout");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
         }
+
+
+
     }
 }
